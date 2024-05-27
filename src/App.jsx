0014899973b9a4d5e228b8a025a65832b8515b64
youhei-0009 <57719055+youhei-0009@users.noreply.github.com>
2024-05-27 19:29:19 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { PrimaryButton } from './components/atoms/button/PrimaryButton';
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <PrimaryButton>Button</PrimaryButton>
+      </div>
+    </>
+  );
+}
---
+++ src/components/atoms/button/PrimaryButton.jsx
@@ -0,0 +1,8 @@
+export const PrimaryButton = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <button>{children}</button>
+    </>
+  );
+};
